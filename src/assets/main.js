6a6a337fc3f8af5f41eb5d237bd06afa1367e36b
pham-huyson87 <pham.huyson87@hotmail.com>
2017-10-29 16:18:41 -0400

    Task 16 - Continue Play Condition
    Modify the guess function to show a message to the user to continue to guess if there is still attempts left

--- src/assets/main.js
+++ src/assets/main.js
@@ -20,6 +20,8 @@ function guess() {
     } else {
         if (attempt.value >= 10) {
             setMessage("You Lose! :(");
+        } else {
+            setMessage("Incorrect, try again.");
         }
     }
 }
