02bede1e14ffa83ff164d2d9bebf5dedbc1f1013
pham-huyson87 <pham.huyson87@hotmail.com>
2017-10-28 22:26:08 -0400

    Task 8 - Only set the answer and attempt hidden inputs when they aren't already set
    Set the random number only at the beginning

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,10 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    
+    if (answer === '' || attempt === '') {
+        setHiddenFields();
+    }
 }
 
 function setHiddenFields() {
